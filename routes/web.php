8916aecec9739df8848b45c322c8084f73882c55
RIZKY ALVIO PURBA <rizkyalprb@gmail.com>
2024-12-08 23:16:35 +0700

    menggunakan bootstrap 5

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,16 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('about', function () {
-    return view('about');
-});
-
 Route::get('mahasiswa', function () {
     return view ('mahasiswa');
 });
 
-Route::get('profile', function () {
-    $nama = ''; 
-    return view('profile', compact('nama'));
+Route::get('about', function () {
+    return view('about');
 });
 
-Route::get('mahasiswa', function () )
+Route::get('profile', function () {
+    $nama = 'jefri'; 
+    // return view('profile',compact('nama'));
+    return view('profile')->with('nama', $nama);
+});
