824cf2c7ba039b181a67e6b481710be3f32507b6
RIZKY ALVIO PURBA <rizkyalprb@gmail.com>
2024-12-09 00:14:07 +0700

    belajar for

--- routes/web.php
+++ routes/web.php
@@ -32,9 +32,7 @@ Route::get('profile', function () {
 });
 
 Route::get('array', function(){
-    $nilai_awal = 1;
-    while ($nilai_awal <= 5) {
-    echo 'Hello Laravel'. $nilai_awal . 'x<br>';
-    $nilai_awal++;
+   for ($i=1; $i <= 5; $i++) { 
+    echo 'Hello World' . $i . 'x<br>';
    }
 });
