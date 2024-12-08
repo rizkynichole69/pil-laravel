88d90d1ac46479b9990f8f3822f3324217863dd3
RIZKY ALVIO PURBA <rizkyalprb@gmail.com>
2024-12-08 22:35:11 +0700

    PUSH pertama saya untuk project laravel kedalam github

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('mahasiswa', function () {
+    return view ('mahasiswa');
+});
+
+Route::get('profile', function () {
+    $nama = ''; 
+    return view('profile', compact('nama'));
+});
+
+Route::get('mahasiswa', function () )
